d149003f7e9fa753b4084e8755d69231e9c028a8
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-22 15:33:50 -0400

    feat: add toggle habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -64,4 +64,51 @@ export const appRoutes = async (app: FastifyInstance) => {
 
     return { possibleHabits, completedHabits };
   });
+
+  app.patch('/habit/:id/toggle', async (request) => {
+    const toggleParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleParams.parse(request.params);
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 };
